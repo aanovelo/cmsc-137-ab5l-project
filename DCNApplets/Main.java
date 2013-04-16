7aa367a9ccb450a536509417e2aed1c03c81464c
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2013-04-16 16:09:34 +0800

    refactor

--- DCNApplets/Main.java
+++
@@ -1,5 +0,0 @@
-package ph.edu.uplb.ics.srg;
-
-public class Main {
-
-}
---
+++ DCNApplets/src/ph/edu/uplb/ics/srg/Main.java
@@ -0,0 +1,8 @@
+package ph.edu.uplb.ics.srg;
+
+public class Main {
+	public static void main(String args[])
+	{
+		System.out.println("Hello World!");
+	}
+}
