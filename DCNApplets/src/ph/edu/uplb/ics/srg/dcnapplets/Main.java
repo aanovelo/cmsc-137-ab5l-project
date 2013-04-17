158829399208586cfcabded9b9dea322e8589769
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2013-04-17 18:05:11 +0800

    added sinusoid

--- DCNApplets/src/ph/edu/uplb/ics/srg/dcnapplets/Main.java
+++ DCNApplets/src/ph/edu/uplb/ics/srg/dcnapplets/Main.java
@@ -1,8 +1,21 @@
 package ph.edu.uplb.ics.srg.dcnapplets;
 
-public class Main {
+import java.awt.Rectangle;
+
+import javax.swing.JFrame;
+
+public class Main extends JFrame {
+	
+	public Main(){
+		this.setTitle("Data Communications Applets");
+		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		this.setVisible(true);
+		this.setBounds(new Rectangle(640,480));
+	}
+	
+	
 	public static void main(String args[])
 	{
-		System.out.println("Hello World!");
+		new Main();
 	}
 }
---
+++ DCNApplets/src/ph/edu/uplb/ics/srg/dcnapplets/Sinusoid.java
@@ -0,0 +1,15 @@
+package ph.edu.uplb.ics.srg.dcnapplets;
+
+public class Sinusoid {
+	float amplitude;
+	float frequency;
+	float phase;
+	
+	public Sinusoid(float amplitude,float frequency,float phase)
+	{
+		this.amplitude = amplitude;
+		this.frequency = frequency;
+		this.phase = phase;
+	}
+	
+}
