e74ab0a675a8efd5f551d97e63d87910918436ad
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2013-06-04 15:53:02 +0800

    classes

--- DCNApplets/quizard/index.php
+++ DCNApplets/quizard/index.php
@@ -1,57 +1,9 @@
 <?php 
 
-class Question  
-{  
-   public $questionId;
-   public $text;
-   public $image;
-   public $choice1;
-   public $choice2;
-   public $choice3;
-   public $choice4;
-   public $answer;
-         
-}
+include 'classes.php';
 
-class Quiz
-{
-   public $quizId;
-   public $title;
-   public $description;
-   public $numItems;
-}
-
-class QuizSession
-{
-   public $sessionId;
-   public $result;
-   public $num_items;
-   
-   public function start_session()
-   {
-   
-   }
-   
-   public function end_session()
-   {
-   
-   }
-   
-   public function taker_name()
-   {
-   
-   }
-   
-   public function randomize()
-   {
-   
-   }
-}
   
 $obj = new Question;  
-
 var_dump($obj);
   
-
-
 ?>
