e19e54f759bcc4dddd0bf1f2141eb5ac22358b78
jachermocilla <jachermocilla@openstack-clc.(none)>
2014-03-26 16:16:58 +0800

    edit

--- jach-phd/peak-two-cloud/tools/vcluster/init-master.sh
+++ jach-phd/peak-two-cloud/tools/vcluster/init-master.sh
@@ -2,4 +2,6 @@
 #echo "Hello World from Master!"
 #echo "Hello World from Master!" > /root/master.txt
 #echo -n "Master IP Address: "
-/sbin/ifconfig eth0 | sed -n 's/.*inet *addr:\([0-9\.]*\).*/\1/p' > /root/hosts.master
+MASTER_IP=`/sbin/ifconfig eth0 | sed -n 's/.*inet *addr:\([0-9\.]*\).*/\1/p'`
+
+echo "$MASTER_IP  jach_master" > /root/hosts.master
