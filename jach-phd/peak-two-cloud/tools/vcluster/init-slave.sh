20edfaf8dfa4021299f56cf5df62652d11326058
jachermocilla <jachermocilla@openstack-clc.(none)>
2014-03-28 13:00:14 +0800

    edits

--- jach-phd/peak-two-cloud/tools/vcluster/init-slave.sh
+++ jach-phd/peak-two-cloud/tools/vcluster/init-slave.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "Executing initialization script for slave..."
 MASTER_IP=10.0.3.231
+SLAVE_NAME=jach-slave-1
 
 echo "Unmounting /mirror"
 umount /mirror
@@ -12,7 +13,7 @@ SLAVE_IP=`/sbin/ifconfig eth0 | sed -n 's/.*inet *addr:\([0-9\.]*\).*/\1/p'`
 
 echo $SLAVE_IP
 
-echo "$SLAVE_IP  jach-slave-1" >> /mirror/hosts.mpi
+echo "$SLAVE_IP  $SLAVE_NAME" >> /mirror/hosts.mpi
 
 
 
