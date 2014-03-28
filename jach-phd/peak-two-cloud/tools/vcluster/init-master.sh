6e907315e2b6c3191ca514672d5f9e10607323ff
jachermocilla <jachermocilla@openstack-clc.(none)>
2014-03-28 11:55:26 +0800

    edits

--- jach-phd/peak-two-cloud/tools/vcluster/init-master.sh
+++ jach-phd/peak-two-cloud/tools/vcluster/init-master.sh
@@ -8,3 +8,5 @@ MASTER_IP=`/sbin/ifconfig eth0 | sed -n 's/.*inet *addr:\([0-9\.]*\).*/\1/p'`
 echo "$MASTER_IP  jach-master" > /mirror/hosts.mpi
 chown mpiuser.mpiuser /mirror/hosts.mpi
 chmod 777 /mirror/hosts.mpi
+rm /etc/hosts
+ln -s /mirror/hosts.mpi /etc/hosts
