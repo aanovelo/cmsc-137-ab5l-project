a28483b2cea3975c9e4111438c72580be3817669
jachermocilla <jachermocilla@openstack-clc.(none)>
2014-03-28 10:42:28 +0800

    edits

--- jach-phd/peak-two-cloud/tools/vcluster/init-slave.sh
+++ jach-phd/peak-two-cloud/tools/vcluster/init-slave.sh
@@ -4,6 +4,7 @@ echo "Executing initialization script for slave..."
 
 MASTER_IP=10.0.3.231
 
+umount /mirror
 mount $MASTER_IP:/mirror /mirror
 
 SLAVE_IP=`/sbin/ifconfig eth0 | sed -n 's/.*inet *addr:\([0-9\.]*\).*/\1/p'`
