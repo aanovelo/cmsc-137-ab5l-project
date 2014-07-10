b44acc5b7869cb996033c226749e0677094f0683
jachermocilla <jachermocilla@openstack-clc.(none)>
2014-07-10 16:00:20 +0800

    edits

--- jach-phd/peak-two-cloud/tools/vhadoop/init-master.sh
+++ jach-phd/peak-two-cloud/tools/vhadoop/init-master.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "Executing initialization script for master..."
-MASTER_NAME=wordcount-hadoop-master
+MASTER_NAME=sample-hadoop-master
 MASTER_IP=`/sbin/ifconfig eth0 | sed -n 's/.*inet *addr:\([0-9\.]*\).*/\1/p'`
 
 echo "$MASTER_NAME" > /etc/hostname
@@ -9,7 +9,7 @@ echo "$MASTER_IP  $MASTER_NAME" >> /etc/hosts
 echo "$MASTER_NAME" > /home/hduser/hadoop/etc/hadoop/slaves
 
 su - hduser
-sed -i 's/MASTER_HOSTNAME/wordcount-hadoop-master/g' /home/hduser/hadoop/etc/hadoop/yarn-site.xml
+sed -i 's/MASTER_HOSTNAME/sample-hadoop-master/g' /home/hduser/hadoop/etc/hadoop/yarn-site.xml
 
 #clear data node directory
 rm -fr /home/hduser/mydata/hdfs/*
--- jach-phd/peak-two-cloud/tools/vhadoop/init-slave.sh
+++ jach-phd/peak-two-cloud/tools/vhadoop/init-slave.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "Executing initialization script for slave..."
 MASTER_IP=192.168.10.2
-SLAVE_NAME=wordcount-hadoop-slave-11
-MASTER_NAME=wordcount-hadoop-master
+SLAVE_NAME=sample-hadoop-slave-2
+MASTER_NAME=sample-hadoop-master
 
 SLAVE_IP=`/sbin/ifconfig eth0 | sed -n 's/.*inet *addr:\([0-9\.]*\).*/\1/p'`
 
@@ -10,9 +10,9 @@ echo "$SLAVE_NAME" > /etc/hostname
 echo "$SLAVE_IP  $SLAVE_NAME" > /etc/hosts
 echo "$MASTER_IP  $MASTER_NAME" >> /etc/hosts
 
-ssh 192.168.10.2 "echo '$SLAVE_IP  wordcount-hadoop-slave-11' >> /etc/hosts" 
-ssh 192.168.10.2 "echo 'wordcount-hadoop-slave-11' >> /home/hduser/hadoop/etc/hadoop/slaves" 
+ssh 192.168.10.2 "echo '$SLAVE_IP  sample-hadoop-slave-2' >> /etc/hosts" 
+ssh 192.168.10.2 "echo 'sample-hadoop-slave-2' >> /home/hduser/hadoop/etc/hadoop/slaves" 
 
 su - hduser
-sed -i 's/MASTER_HOSTNAME/wordcount-hadoop-master/g' /home/hduser/hadoop/etc/hadoop/yarn-site.xml
+sed -i 's/MASTER_HOSTNAME/sample-hadoop-master/g' /home/hduser/hadoop/etc/hadoop/yarn-site.xml
 rm -fr /home/hduser/mydata/hdfs/*
