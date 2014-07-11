a46ee0b49fc4a1bbdb1e9a166098e6b7ac0e5cc2
jachermocilla <jachermocilla@openstack-clc.(none)>
2014-07-11 15:13:45 +0800

    edits

--- jach-phd/peak-two-cloud/tools/vhadoop/init-slave.sh
+++ jach-phd/peak-two-cloud/tools/vhadoop/init-slave.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "Executing initialization script for slave..."
-MASTER_IP=192.168.10.2
-SLAVE_NAME=sample-hadoop-slave-2
-MASTER_NAME=sample-hadoop-master
+MASTER_IP=192.168.10.3
+SLAVE_NAME=rhea-hadoop-slave-2
+MASTER_NAME=rhea-hadoop-master
 
 SLAVE_IP=`/sbin/ifconfig eth0 | sed -n 's/.*inet *addr:\([0-9\.]*\).*/\1/p'`
 
@@ -10,9 +10,9 @@ echo "$SLAVE_NAME" > /etc/hostname
 echo "$SLAVE_IP  $SLAVE_NAME" > /etc/hosts
 echo "$MASTER_IP  $MASTER_NAME" >> /etc/hosts
 
-ssh 192.168.10.2 "echo '$SLAVE_IP  sample-hadoop-slave-2' >> /etc/hosts" 
-ssh 192.168.10.2 "echo 'sample-hadoop-slave-2' >> /home/hduser/hadoop/etc/hadoop/slaves" 
+ssh 192.168.10.3 "echo '$SLAVE_IP  rhea-hadoop-slave-2' >> /etc/hosts" 
+ssh 192.168.10.3 "echo 'rhea-hadoop-slave-2' >> /home/hduser/hadoop/etc/hadoop/slaves" 
 
 su - hduser
-sed -i 's/MASTER_HOSTNAME/sample-hadoop-master/g' /home/hduser/hadoop/etc/hadoop/yarn-site.xml
+sed -i 's/MASTER_HOSTNAME/rhea-hadoop-master/g' /home/hduser/hadoop/etc/hadoop/yarn-site.xml
 rm -fr /home/hduser/mydata/hdfs/*
